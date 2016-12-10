de079170249afb6d9c6cf5736cf098220576d8e2
xamgore <xamgore@ya.ru>
2016-12-10 19:25:21 +0300

    Get rid of Shakespeare's templates

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,8 +3,5 @@ module Handler.Home where
 import Import
 
 
-getHomeR :: Handler Html
-getHomeR = do
-    defaultLayout $ do
-        setTitle "Index"
-        $(widgetFile "index")
+getHomeR :: Handler ()
+getHomeR = sendFile "text/html" "static/index.html"
