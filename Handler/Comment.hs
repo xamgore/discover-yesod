bc73e5dec16700e79b7e76a1d7d8a4542da5d7b3
xamgore <xamgore@ya.ru>
2016-12-10 16:56:55 +0300

    Move JSON example to `comment` route

--- Handler/Comment.hs
+++ Handler/Comment.hs
@@ -2,13 +2,32 @@ module Handler.Comment where
 
 import Import
 
+
+-- This is a handler function for the GET request method on the HomeR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+
+
+getCommentR :: Handler Html
+getCommentR = do
+    defaultLayout $ do
+        setTitle "Comments"
+        $(widgetFile "homepage")
+
+
 postCommentR :: Handler Value
 postCommentR = do
-    -- requireJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
+    -- requireJsonBody will parse the request body into the appropriate type,
+    -- or return a 400 status code if the request JSON is invalid.
     -- (The ToJSON and FromJSON instances are derived in the config/models file).
     comment <- (requireJsonBody :: Handler Comment)
 
-    -- The YesodAuth instance in Foundation.hs defines the UserId to be the type used for authentication.
+    -- The YesodAuth instance in Foundation.hs defines the UserId
+    -- to be the type used for authentication.
     maybeCurrentUserId <- maybeAuthId
     let comment' = comment { commentUserId = maybeCurrentUserId }
 
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,24 +1,10 @@
 module Handler.Home where
 
 import Import
-import Text.Julius (RawJS (..))
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
-        setTitle "Main page"
-        $(widgetFile "homepage")
-
-postHomeR :: Handler Html
-postHomeR = do
-    defaultLayout $ do
-        setTitle "Main page"
-        $(widgetFile "homepage")
+        setTitle "Index"
+        $(widgetFile "index")
