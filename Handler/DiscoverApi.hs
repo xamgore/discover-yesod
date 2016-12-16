5f44bea834d7abf60fbbd9c9454016209b6ddf4c
Vsevolod Kvachev <rasielll@gmail.com>
2016-12-17 01:40:36 +0300

    request params

--- Handler/DiscoverApi.hs
+++ Handler/DiscoverApi.hs
@@ -2,27 +2,36 @@ module Handler.DiscoverApi where
 
 import Import
 
-getUsersR :: Handler ()
-getUsersR = sendFile "text/html" "static/index.html"
+getUsersR :: Handler Html
+getUsersR = do
+    pageMaybe <- lookupGetParam "page"
+    userMaybe <- lookupGetParam "friends_of"
+    case userMaybe 
+        Just user -> sendFile "text/html" ("static/index.html" ++ user)
+        Nothing   -> sendFile "text/html" "static/index.html"
 
-getUserR :: Text -> Handler ()
+getUserR :: Text -> Handler Html
 getUserR userId = sendFile "text/html" "static/index.html"
 
-getPlacesR :: Handler ()
-getPlacesR = sendFile "text/html" "static/index.html"
+getPlacesR :: Handler Html
+getPlacesR = do
+    pageMaybe <- lookupGetParam "page"
+    positionMaybe <- lookupGetParam "nearby_xy"
+    userMaybe <- lookupGetParam "visited_by"
+    sendFile "text/html" "static/index.html"
 
-postPlacesR :: Handler ()
+postPlacesR :: Handler Html
 postPlacesR = sendFile "text/html" "static/index.html"
 
-getPlaceR :: Text -> Handler ()
+getPlaceR :: Text -> Handler Html
 getPlaceR placeId = sendFile "text/html" "static/index.html"
 
-postDiscoverPlaceR :: Text -> Handler ()
+postDiscoverPlaceR :: Text -> Handler Html
 postDiscoverPlaceR placeId = sendFile "text/html" "static/index.html"
 
-getPlacesPhotosR :: Text -> Handler ()
+getPlacesPhotosR :: Text -> Handler Html
 getPlacesPhotosR placeId = sendFile "text/html" "static/index.html"
 
-postPlacesPhotosR :: Text -> Handler ()
+postPlacesPhotosR :: Text -> Handler Html
 postPlacesPhotosR placeId = sendFile "text/html" "static/index.html"
 
